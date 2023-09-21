ce69325d2cdc8452b77dcee4402f06889a71ad13
astrionic <32393038+astrionic@users.noreply.github.com>
2023-09-21 20:21:32 +0200

    Update model

--- src/model.tsx
+++ src/model.tsx
@@ -49,7 +49,7 @@ export class Product {
 export function getDepositPrice(deposit: Deposit): number {
     switch (deposit) {
         case Deposit.CUP:
-            return 2;
+            return 5;
         case Deposit.MUG:
             return 10;
         case Deposit.BOTTLE:
@@ -59,27 +59,16 @@ export function getDepositPrice(deposit: Deposit): number {
     }
 }
 
-export const allProducts : Product[]  = [
-    new Product("Bier", '#cfefff', ProductType.DRINK, 6, Deposit.CUP, AgeRestriction.SIXTEEN_PLUS),
-    new Product("Craftbeer", '#cfefff',ProductType.DRINK, 8, Deposit.CUP, AgeRestriction.SIXTEEN_PLUS),
-    new Product("Moscht", '#cfefff',ProductType.DRINK, 7, Deposit.NONE, AgeRestriction.SIXTEEN_PLUS),
-    new Product("Softdrink", '#ffffff',ProductType.DRINK, 5, Deposit.CUP, AgeRestriction.NONE),
-    new Product("Energy Drink", '#ffffff',ProductType.DRINK, 6, Deposit.NONE, AgeRestriction.NONE),
-    new Product("Bier Alkoholfrei", '#ffffff',ProductType.DRINK, 6, Deposit.NONE, AgeRestriction.NONE),
-    new Product("Mule", '#cfffdf',ProductType.DRINK, 15, Deposit.MUG, AgeRestriction.EIGHTEEN_PLUS),
-    new Product("Longdrink", '#dfdfef',ProductType.DRINK, 14, Deposit.CUP, AgeRestriction.EIGHTEEN_PLUS),
-    new Product("Shot", '#ffcfcf',ProductType.DRINK, 5, Deposit.NONE, AgeRestriction.EIGHTEEN_PLUS),
-    new Product("Virgin Mule", '#cfffdf',ProductType.DRINK, 10, Deposit.MUG, AgeRestriction.NONE),
-    new Product("Virgin Sunrise", '#dfdfef',ProductType.DRINK, 10, Deposit.CUP, AgeRestriction.NONE),
-    new Product("Shot Flasche", '#ffcfcf',ProductType.DRINK, 50, Deposit.BOTTLE, AgeRestriction.EIGHTEEN_PLUS),
-    new Product("Aperol / Hugo", '#ffefbf',ProductType.DRINK, 12, Deposit.CUP, AgeRestriction.EIGHTEEN_PLUS),
-    new Product("Gsprützte Wiisse", '#ffefbf',ProductType.DRINK, 8, Deposit.CUP, AgeRestriction.SIXTEEN_PLUS),
-    new Product("Cüpli", '#ffefbf',ProductType.DRINK, 7, Deposit.CUP, AgeRestriction.SIXTEEN_PLUS),
-    new Product("Burger", '#ffcfef',ProductType.FOOD, 14, Deposit.NONE, AgeRestriction.NONE),
-    new Product("Nachos", '#ffcfef',ProductType.FOOD, 8, Deposit.NONE, AgeRestriction.NONE),
-    new Product("Glacé", '#ffcfef',ProductType.FOOD, 6, Deposit.NONE, AgeRestriction.NONE),
-    new Product("Kaffee", '#dfbfbf',ProductType.DRINK, 4, Deposit.NONE, AgeRestriction.NONE),
-    new Product("Eis-Kaffee", '#dfbfbf',ProductType.DRINK, 6, Deposit.CUP, AgeRestriction.NONE),
-    new Product("Bier Dose", '#cfefff',ProductType.DRINK, 6, Deposit.NONE, AgeRestriction.SIXTEEN_PLUS),
+export const allProducts: Product[] = [
+    new Product("Softdrink", '#ffffff',ProductType.DRINK, 5, Deposit.NONE, AgeRestriction.NONE),
+    new Product("Sonnwendig (alkoholfrei)", '#cfefff',ProductType.DRINK, 4, Deposit.NONE, AgeRestriction.NONE),
+    new Product("Moscht (alkoholfrei)", '#cfefff',ProductType.DRINK, 5, Deposit.NONE, AgeRestriction.NONE),
+    new Product("Köhlerbier offen", '#cfefff', ProductType.DRINK, 6, Deposit.CUP, AgeRestriction.SIXTEEN_PLUS),
+    new Product("Köhlerbier", '#cfefff', ProductType.DRINK, 5, Deposit.NONE, AgeRestriction.SIXTEEN_PLUS),
+    new Product("Moscht", '#cfefff',ProductType.DRINK, 5, Deposit.NONE, AgeRestriction.SIXTEEN_PLUS),
+    new Product("Wein", '#ffcfcf', ProductType.DRINK, 16, Deposit.NONE, AgeRestriction.SIXTEEN_PLUS),
+    new Product("Burger", '#ffcfef',ProductType.FOOD, 12, Deposit.NONE, AgeRestriction.NONE),
+    new Product("Wurst", '#ffcfef',ProductType.FOOD, 6, Deposit.NONE, AgeRestriction.NONE),
+    new Product("Pommes Frites", '#ffcfef',ProductType.FOOD, 12, Deposit.NONE, AgeRestriction.NONE),
 ];
 
